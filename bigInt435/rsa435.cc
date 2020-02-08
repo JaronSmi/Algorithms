8dd6d689d8761882210ad4101710960f65678eda
JaronSmi <jms642@zips.uakron.edu>
2020-02-08 13:51:18 -0500

    debugging and minor adjustments

--- bigInt435/rsa435.cc
+++ bigInt435/rsa435.cc
@@ -15,7 +15,7 @@
 
 
 BigUnsigned& generate_prime(const int);
-void new_random(BigUnsigned &, int);
+BigUnsigned& new_random(BigUnsigned &, int);
 void store_values(const BigUnsigned &, const BigUnsigned &, const char[]);
 BigUnsigned& generate_e(const BigUnsigned &);
 void display_value(const BigUnsigned &, const char[]);
@@ -27,7 +27,7 @@ int main(){
 	//seed random generator
 	unsigned seed = time(0);
 	srand(seed);
-	size_of_prime = 512;
+	int size_of_prime = 512;
 	//generate p val
 	BigUnsigned p = generate_prime(size_of_prime);
 
@@ -38,11 +38,11 @@ int main(){
 	store_values(p, q, "p_q.txt");
 
 	BigUnsigned n = p * q;
-	display_value(n, 'n');
+	display_value(n, "n");
 	BigUnsigned phi_n = (p-1) * (q-1);
 	display_value(phi_n, "phi of n");
 
-	BigUnsigned e =	generate_e(phi_n, e);
+	BigUnsigned e =	generate_e(phi_n);
 	store_values(e, n, "e_n.txt");
 	display_value(e, "e");
 	BigUnsigned d = modinv(e, phi_n);
@@ -53,77 +53,80 @@ int main(){
 	return 0;
 }
 
-void generate_prime(const int size){
+BigUnsigned& generate_prime(const int size){
       std::cout << "Generating prime of size " << size << " bits...\n";
       BigUnsigned exponent;
       BigUnsigned base;
       BigUnsigned a = BigUnsigned(1);
-      BigUnsigned canidate = BigUnsigned(1);
-      new_random(a, (size-1));
+      BigUnsigned candidate = BigUnsigned(1);
       BigUnsigned result;
       bool isprime = false;
       // Iterate until a valid prime is generated
       while (!isprime)
       {
-            // Continue generating new canidates until not even
-            // Reduces unecessary iterations of fermat test
+      	std::cout<< "Hello \n";
             do
             {
-                  new_random(canidate, size);
-            } while (canidate % 2 == 0);
+                  candidate = new_random(candidate, size);
+                  std::cout << "new canidate found";
+            } while (candidate % 2 == 0);
             // Conduct fermat test  with 3 different a values
-            exponent = canidate - 1;
+            exponent = candidate - 1;
             for (int i = 0; i < 3; ++i)
             {
-                  result = modexp(a, exponent, canidate);
+                  result = modexp(a, exponent, candidate);
                   // failed fermat, genereate new canidate
                   if (result != 1)
                   {
-                        break;
+                  		i = i + 2;
                   }
                   if (i < 2)
                   {
                         // test with new a
-                        new_random(a, (size-1));
+                        a = new_random(a, (size-1));
                   }
                   // All 3 values of a passed
                   else
                   {
                         isprime = true;
+                        return candidate;
                   }
             }
       }
 }
 
-void new_random(BigUnsigned & canidate, int size)
+BigUnsigned& new_random(BigUnsigned & candidate, int size)
 {
       // constants for random number generation
       const int MIN_VAL = 1;
       const int MAX_VAL = 10;
-      canidate = (rand() % (MAX_VAL - MIN_VAL + 1)) + MIN_VAL;
+      candidate = (rand() % (MAX_VAL - MIN_VAL + 1)) + MIN_VAL;
       for (int i = 1; i < size; i++)
       {
-            canidate = canidate * 10 + ((rand() % (MAX_VAL - MIN_VAL + 1)) + MIN_VAL);
+            candidate = candidate * 10 + ((rand() % (MAX_VAL - MIN_VAL + 1)) + MIN_VAL);
       }
+      return candidate;
+
 }
 void store_values(const BigUnsigned &a, const BigUnsigned &b, const char file[])
 	{
 		std::ofstream output;
-		output.open((file);
+		output.open((file));
 		output << a << "\n";
 		output << b;
 		output.close();
 	}
 BigUnsigned& generate_e(const BigUnsigned &phi_n)
 {
-	BigUnsigned e = 13;
+	BigUnsigned e = 5;
 	while((gcd(phi_n, e) !=1) && (e < phi_n))
 	{
 		++e;
 	}
+	return e;
 }
 void display_value(const BigUnsigned &a, const char var[])
 {
-	std::cout << "The value of " << var << "is:: \n"<<
-			  << value << std::endl;
+	std::cout << "The value of " << var << "is:: \n"
+			  << a << std::endl;
 }
