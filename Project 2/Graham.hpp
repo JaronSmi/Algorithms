c93bc1395fb805861bf238aed70acc2b7044066d
JaronSmi <jms642@zips.uakron.edu>
2020-03-10 11:48:39 -0400

    Abstract Graham Scan

--- Project 2/Graham.hpp
+++ Project 2/Graham.hpp
@@ -1,5 +1,11 @@
-// Prototypes for Graham Scan
-int compare(const void*, const void*);
-int direction(coordinate_pair, coordinate_pair, coordinate_pair);
-int squaredDist(coordinate_pair, coordinate_pair);
-coordinate_pair second_top(std::stack<coordinate_pair> &);
+struct Point 
+{ 
+   int x, y; 
+}; 
+
+Point nextToTop(stack<Point> &S);
+int swap(Point &p1, Point &p2);
+int distSq(Point p1, Point p2);
+int orientation(Point p, Point q, Point r);
+int compare(const void *vp1, const void *vp2);
+void GrahamScan(Point points[], int n);
