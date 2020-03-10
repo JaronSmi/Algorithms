5dc1dec2dd8658a3e27dec5518dc5111c435eb6b
JaronSmi <jms642@zips.uakron.edu>
2020-03-10 11:49:28 -0400

    Abstract Jarvis March

--- Project 2/Jarvis.hpp
+++ Project 2/Jarvis.hpp
@@ -1,4 +1,2 @@
-// Prototypes for Jarvis March
-std::set<coordinate_pair> jarvis_hull(std::vector<coordinate_pair> &, int);
-int cross_product(coordinate_pair, coordinate_pair, coordinate_pair);
-int distance(coordinate_pair, coordinate_pair, coordinate_pair);
+int orientation(Point p, Point q, Point r);
+void JarvisMarch(Point points[], int n);
