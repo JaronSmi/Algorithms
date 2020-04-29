163e5b76da7aef1087000c8840626d24aafb7075
JaronSmi <jms642@zips.uakron.edu>
2020-04-29 11:56:55 -0400

    Add description and method declarations

---
+++ jsmith_3/seam_carving.cpp
@@ -0,0 +1,24 @@
+/* Jaron Smith - 4203199
+	Algorithms Project 3 - Seam Carving
+
+	Description:
+	This project is executed with a valid pgm file name and 2 parameters
+	for number of vertical and horizontal seams to remove.
+
+	Seams are removed by calculating energy grid then Cumulative
+	energy grid.
+	
+	The grid is resized for each seam removal and remaining vals are
+	transferred to new grid.
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <fstring>
+
+void read_pgm_file(const std::string & int** &, int &, int &, int &);
+void process_image_seams(int** &, int &, int &);
+void output_processed_pgm(int** const &, const int, const int, const int, const std::string &);
+void clockwise_ninety(int** &, int &, int &);
+void counter_clockwise_ninety(int** &, int &, int &);
